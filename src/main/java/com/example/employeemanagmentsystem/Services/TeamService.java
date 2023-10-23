d914d4e7a1a915a6f59e95cece02e75a396d91dc
MWnuku <mikolajwnuk02@gmail.com>
2023-10-23 20:26:00 +0200

    applied controller-service-repository design pattern

--- src/main/java/com/example/employeemanagmentsystem/Services/TeamService.java
+++ src/main/java/com/example/employeemanagmentsystem/Services/TeamService.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @Service
 public class TeamService{
-	private TeamRepository teamRepository;
+	private final TeamRepository teamRepository;
 
 	public TeamService(TeamRepository teamRepository){
 		this.teamRepository = teamRepository;
